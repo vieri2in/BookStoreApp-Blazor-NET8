542429082d966ed5723339cf3195f1260ec28175
abc123 <abc123@DESKTOP-9VG4RA5>
2024-04-03 18:23:47 -0700

    Add project files.

---
+++ BookStoreAppApi/Program.cs
@@ -0,0 +1,37 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog((HostBuilderContext context, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration.WriteTo.Console().ReadFrom.Configuration(context.Configuration);
+});
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policyBuild =>
+    {
+        policyBuild.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
+    });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
